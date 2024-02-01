84a288fdcfc7e38df1ad34f56963a67c41b5a75e
swarnatonse <swarnatonse@gmail.com>
2024-02-01 10:39:36 +0530

    add high score feature

--- scoreboard.py
+++ scoreboard.py
@@ -1,18 +1,38 @@
 from turtle import Turtle
 
+FONT = ("Arial", 24, "normal")
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.high_score = None
         self.score = 0
         self.penup()
         self.goto(0, 270)
         self.color("white")
         self.hideturtle()
+        self.load_high_score()
         self.update_scoreboard()
 
+    def load_high_score(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align="center", font=FONT)
+
+    def update_high_score(self):
+        with open("data.txt", mode="w") as file:
+            file.write(str(self.high_score))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.update_high_score()
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
@@ -21,5 +41,4 @@ class Scoreboard(Turtle):
 
     def game_over(self):
         self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Arial", 30, "normal"))
-
+        self.write("GAME OVER", align="center", font=FONT)
